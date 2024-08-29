24badf3667552b286219838cb364c0a9df985123
Tai Silva30 <tatihsilva16@gmail.com>
2024-08-28 21:57:51 -0300

    incluindo resultado relacionais

---
+++ resultado_relacionais.py
@@ -0,0 +1,26 @@
+a = 5
+b = 3
+
+#igualdade
+resultado = a == b
+print(resultado)
+
+#diferença
+resultado = a != b
+print(resultado)
+
+#maior que 
+resultado = a > b
+print(resultado)
+
+#menor que
+resultado = a < b
+print(resultado)
+
+#maior ou igual 
+resultado = a >= b
+print(resultado)
+
+#menor ou igual 
+resultado = a <= b
+print(resultado)
